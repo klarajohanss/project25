9423ed90e3f7526c0bf9ca4f006595160108ef25
klarajohanss <klara.johansson@elev.ga.ntig.se>
2025-02-04 10:10:45 +0100

    loopat ut data från db
    - lagt till testdata i db
    - hämtat datan från produkt-tabellen
    - loopat ut datan i produkt-slimfilen
    - lagt till get routes för alla slimfiler

--- app.rb
+++ app.rb
@@ -2,9 +2,37 @@ require 'sinatra'
 require 'slim'
 require 'sqlite3'
 require 'sinatra/reloader'
-require 'becrypt'
+require 'bcrypt'
 enable :sessions
 
 get('/home') do
     slim(:index)
+end
+
+get('/show_login') do
+    slim(:index)
+end
+
+get('/show_varukorg') do
+    slim(:varukorg)
+end
+
+get('/show_produkter') do
+    db = SQLite3::Database.new("db/webbshop.db")
+    db.results_as_hash = true
+    result = db.execute("SELECT * FROM produkt")
+
+    slim(:produkter, locals:{produkt:result})
+end
+
+get('/show_produkt') do
+    slim(:produkt)
+end
+
+get('/show_om_oss') do
+    slim(:om_oss)
+end
+
+get('/show_kontakt') do
+    slim(:kontakt)
 end

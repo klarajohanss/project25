33dc2f7f17bea44faf58d695fc17f72558f78d72
klarajohanss <klara.johansson@elev.ga.ntig.se>
2025-05-02 17:34:44 +0200

    model v1

--- model.rb
+++ model.rb
@@ -1,13 +1,13 @@
 require 'sqlite3'
 
-def connect_to_db
-    db = SQLite3::Database.new("db/webbshop.db")
+DB_PATH = "db/webbshop.db"
+
+def db_connection
+    db = SQLite3::Database.new(DB_PATH)
     db.results_as_hash = true
     db
 end
 
 def fetch_two_products
-    db = SQLite3::Database.new("db/webbshop.db")
-    db.results_as_hash = true
-    db.execute("SELECT * FROM products LIMIT 2")
+    db_connection.execute("SELECT * FROM products LIMIT 2")
 end
